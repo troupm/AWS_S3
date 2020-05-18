37149fb996f208a236dd4d95ed43108e9bf74996
tmiller <tmiller@amherst.com>
2020-05-18 16:51:33 -0500

    updated serrvice code to accept filename as route param

--- bpo-s3-pdf-service.js
+++ bpo-s3-pdf-service.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bucket = 'bpo-pdf-test-1'
 // aws
 var AWS = require('aws-sdk')
 var s3 = new AWS.S3({apiVersion: 'latest'})
@@ -14,9 +15,15 @@ var {Base64Encode} = require('base64-stream')
 // I configured mine through the CLI, which persists a config file locally (with secrets) so you don't have to do this stuff live
 // AWS.config.update({region: 'us-east-1', credentials: {YOUR_CREDENTIALS}});
 
-app.listen(port, () => console.log(`S3 PDF Server listening at http://localhost:${port}`))
+app.listen(port, () => {
+    console.log(`S3 PDF Server listening at http://localhost:${port}`)
+    console.log(`Targeting AWS S3 Bucket: ${bucket}`)
+})
 
-app.get('/', (req, res) => {
-    var params = {Bucket: 'bpo-pdf-test-1', Key: 'TestPdf.pdf'}
+app.get('/:filename', (req, res) => {
+    let targetFilename = req.params.filename
+    //let params = {Bucket: 'bpo-pdf-test-1', Key: 'TestPdf.pdf'}
+    console.log(`Fetching file from S3. Bucket: ${bucket} Filename: ${targetFilename}`)
+    let params = {Bucket: bucket, Key: targetFilename}
     s3.getObject(params).createReadStream().pipe(new Base64Encode()).pipe(res)
 })
