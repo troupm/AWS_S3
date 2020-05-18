3bd2ab07d3b4866851260f419822b7a131e5c30e
tmiller <tmiller@amherst.com>
2020-05-18 17:38:31 -0500

    added POST route (WIP)

--- bpo-s3-pdf-service.js
+++ bpo-s3-pdf-service.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bucket = 'bpo-pdf-test-1'
+const bodyParser = require('body-parser');
 // aws
 var AWS = require('aws-sdk')
 var s3 = new AWS.S3({apiVersion: 'latest'})
@@ -15,6 +16,8 @@ var {Base64Encode} = require('base64-stream')
 // I configured mine through the CLI, which persists a config file locally (with secrets) so you don't have to do this stuff live
 // AWS.config.update({region: 'us-east-1', credentials: {YOUR_CREDENTIALS}});
 
+app.use(bodyParser.urlencoded({ extended: true })); // TODO: <-- Untested Copypasta
+
 app.listen(port, () => {
     console.log(`S3 PDF Server listening at http://localhost:${port}`)
     console.log(`Targeting AWS S3 Bucket: ${bucket}`)
@@ -22,8 +25,34 @@ app.listen(port, () => {
 
 app.get('/:filename', (req, res) => {
     let targetFilename = req.params.filename
-    //let params = {Bucket: 'bpo-pdf-test-1', Key: 'TestPdf.pdf'}
     console.log(`Fetching file from S3. Bucket: ${bucket} Filename: ${targetFilename}`)
     let params = {Bucket: bucket, Key: targetFilename}
     s3.getObject(params).createReadStream().pipe(new Base64Encode()).pipe(res)
+})
+
+app.post('/:filename', (req, res) => {
+    let targetFilename = req.params.filename
+    console.log(`Incoming Upload request- Filename: ${targetFilename}`, req.body)
+    console.log(`Request Body:`, req.body)
+    let fileData = req.body // TODO: What kind of Encoding/Decoding/Transformation do I need here to be AWS S3 compatable?
+    // Use S3 ManagedUpload class as it supports multipart uploads
+  let upload = new AWS.S3.ManagedUpload({
+    params: {
+      Bucket: bucket,
+      Key: targetFilename,
+      Body: fileData //,
+      // ACL: "public-read" // TODO: <-- Do I need to specify this?
+    }})
+    let uploadPromise = upload.promise();
+
+    console.log(`Uploading file to S3. Bucket: ${bucket} Filename: ${targetFilename}...`)
+    promise.then(
+        function(data) {
+          alert("File upload successful.");
+          res.sendStatus(200); // TODO: Does this go here?
+        },
+        function(err) {
+          return alert("Error uploading file: ", err.message)
+        }
+      );
 })
